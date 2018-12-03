dad275a8201a2af0013c9a3969e568d63f7d8801
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2018-12-03 13:17:24 +0100

    Initial commit

---
+++ MyGitTestproject/MyGitTestproject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyGitTestproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
